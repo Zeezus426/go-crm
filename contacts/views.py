4427f51eda1d793423953620fb0527cd429e8119
Zeezus426 <zachaldin@outlook.com>
2025-11-07 11:35:11 +1100

    added some styling, and have rendered a few will run some more tests

--- contacts/views.py
+++ contacts/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, redirect, get_object_or_404   
 from .models import Contact
+from django.template.loader import get_template
 # Create your views here.
 
 # Gives you the list of all contacts will intergrate this into the main e-com main
+
 def contact_list(request):
+    try:
+        template = get_template('base.html')
+        print("✅ FOUND: base.html")
+    except Exception as e:
+        print("❌ NOT FOUND: base.html", e)
+
     contacts = Contact.objects.all()
     return render(request, 'contacts/contact_list.html', {'contacts': contacts})
 
@@ -17,7 +25,7 @@ def contact_create(request):
         phone = request.POST.get['phone'])
         contact.save
         return redirect('contact_list')
-    return render(request, 'contacts/contact_form.html')
+    return render(request, 'contacts/contact_create.html')
 
 # Updates the contacts on our end
 def contact_update(request, pk):
@@ -29,7 +37,7 @@ def contact_update(request, pk):
         contact.phone = request.POST.get['phone']
         contact.save()
         return redirect('contact_list')
-    return render(request, 'contacts/contact_form.html', {'contact': contact})
+    return render(request, 'contacts/contact_update.html', {'contact': contact})
 
 
 # Deletes contacts from our database    
@@ -38,4 +46,4 @@ def contact_delete(request, pk):
     if request.method == "POST":
         contact.delete()
         return redirect('contact_list')
-    return render(request, 'contacts/contact_confirm_delete.html', {'contact': contact})    
+    return render(request, 'contacts/contact_delete.html', {'contact': contact})    
