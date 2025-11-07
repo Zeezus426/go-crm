4fbc7dc77b910c7265e78d14f6d46cd86455ca09
Zeezus426 <zachaldin@outlook.com>
2025-11-07 11:04:30 +1100

    added the del contact and the url paths for contacts path

--- contacts/views.py
+++ contacts/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Contact
 # Create your views here.
 
-
+# Gives you the list of all contacts will intergrate this into the main e-com main
 def contact_list(request):
     contacts = Contact.objects.all()
     return render(request, 'contacts/contact_list.html', {'contacts': contacts})
 
-
+# Creates contacts will add more features as we go on
 def contact_create(request):
     if request.method == "POST":
         contact = Contact(
@@ -19,7 +19,7 @@ def contact_create(request):
         return redirect('contact_list')
     return render(request, 'contacts/contact_form.html')
 
-
+# Updates the contacts on our end
 def contact_update(request, pk):
     contact = get_object_or_404(Contact, pk=pk)
     if request.method == "POST":
@@ -30,3 +30,12 @@ def contact_update(request, pk):
         contact.save()
         return redirect('contact_list')
     return render(request, 'contacts/contact_form.html', {'contact': contact})
+
+
+# Deletes contacts from our database    
+def contact_delete(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    if request.method == "POST":
+        contact.delete()
+        return redirect('contact_list')
+    return render(request, 'contacts/contact_confirm_delete.html', {'contact': contact})    
