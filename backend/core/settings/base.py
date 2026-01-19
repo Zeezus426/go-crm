5b4c81e85c035d91ecdf280c43ef0a156d724c47
Zeezus426 <166266991+Zeezus426@users.noreply.github.com>
2026-01-19 14:43:06 +1100

    attempting to do api

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -117,14 +117,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# Email Configuration - Will be overridden in local.py and prod.py
-EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
-EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-
 ANYMAIL = {
     "MAILGUN_API_KEY": config('MAILGUN_API_KEY'),
 }
