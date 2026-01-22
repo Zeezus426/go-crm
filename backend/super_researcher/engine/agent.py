eb25fe180f6e0e3ba103e2094eb542251e304f79
Zeezus426 <166266991+Zeezus426@users.noreply.github.com>
2026-01-22 12:34:32 +1100

    adding the frontend definition

--- backend/super_researcher/engine/agent.py
+++ backend/super_researcher/engine/agent.py
@@ -3,16 +3,17 @@ from google.adk.tools.mcp_tool.mcp_toolset import McpToolset
 from google.adk.tools.mcp_tool.mcp_session_manager import StdioConnectionParams, SseConnectionParams, SseServerParams
 from mcp import StdioServerParameters
 from google.adk.models.lite_llm import LiteLlm
-
 from .prompting import research_prompt
 from pydantic import BaseModel, Field
 from typing import Literal
 from decouple import config
 
+
+GLM_BASE_URL = config("GLM_API_BASE_URL", default="https://api.z.ai/api/paas/v4/")
 # Define the model once and reuse it
 model_name_at_endpoint = "lm_studio/glm-4.6v-flash"
 api_base_url = "http://localhost:1234/v1"
-
+model_name = "openai/GLM-4.7-Flash"
 # Create the LiteLlm model instance
 model = LiteLlm(
     # model = model_name_at_endpoint,
@@ -20,11 +21,21 @@ model = LiteLlm(
     stream = True,
     model = "openai/GLM-4.7-Flash",
     api_key=config("GLM_API_KEY"),
-    base_url="https://api.z.ai/api/paas/v4/",
+    base_url=GLM_BASE_URL,
     response_format={"type": "json_object"}
-
 )
 
+model2 = LiteLlm(
+    # model = model_name_at_endpoint,
+    # base_url=api_base_url,
+    stream = True,
+    model = model_name,
+    api_key=config("GLM_API_KEY"),
+    base_url=GLM_BASE_URL,
+    response_format={"type": "json_object"}
+)
+if model == None:
+    model = model2
 
 
 class ResearchOutput(BaseModel):
@@ -51,8 +62,8 @@ root_agent = LlmAgent(
                     command="npx",
                     args=["-y", "firecrawl-mcp"],
                     env={
-                        "FIRECRAWL_API_URL": "http://localhost:3002",
-                        "FIRECRAWL_API_KEY": "sk-local"
+                        "FIRECRAWL_API_URL": config("FIRECRAWL_API_URL", default="http://localhost:3002"),
+                        "FIRECRAWL_API_KEY": config("FIRECRAWL_API_KEY", default="sk-local")
                     },
                 )
             )
