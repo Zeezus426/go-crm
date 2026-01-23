b92e5b914b085bd435741dd980acac296db21784
YakPort <35947849+YakPort@users.noreply.github.com>
2026-01-23 13:42:14 +1100

    modified entry

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 chmod +x manage.py
 python manage.py migrate
-daphne -b 0.0.0.0 -p 8000  --proxy-headers djecommerce.asgi:application
+daphne -b 0.0.0.0 -p 8000  --proxy-headers core.asgi:application
