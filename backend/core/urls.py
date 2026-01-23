5b33b5d24520588f288fd234160ab9c9cc5303df
YakPort <35947849+YakPort@users.noreply.github.com>
2026-01-23 13:28:59 +1100

    conditioonal on debug

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include, re_path
 from contacts.api import contact_router
@@ -40,7 +41,10 @@ urlpatterns = [
     path('super_researcher/', include('super_researcher.urls')),
 
     path('apex/', include('apex.urls')),
-
-    # Debug to be removed in production
-    path('__debug__/', include('debug_toolbar.urls')),
 ]
+
+# Only include debug toolbar in development
+if settings.DEBUG:
+    urlpatterns += [
+        path('__debug__/', include('debug_toolbar.urls')),
+    ]
